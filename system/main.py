44030ec391eb50b7118a7ed6ae6b50a74015ead5
Athanase Matabaro <athanase678@gmail.com>
2022-05-09 14:33:24 +0300

    variable creation concept

--- system/main.py
+++ system/main.py
@@ -1,10 +1,15 @@
-item1 = 'phone'
-item1_price = 100
-item1_quantity =5
-item1_price_total = item1_price * item1_quantity
+class Item:
+    def calculate_total_price(self, x ,y):
+        return x * y
 
-print(type(item1)) #string
-print(type(item1_price)) # int
-print(type(item1_quantity)) # int
-print(type(item1_price_total)) # int 
+item1 = Item()
+item1.name = "phone"
+item1.price = 100
+item1.quantity = 5
+print(item1.calculate_total_price(item1.price, item1.quantity))
 
+item2 = Item()
+item2.name = "laptop"
+item2.price = 1000
+item2.quantity = 2
+print(item2.calculate_total_price(item2.price, item2.quantity))
