8008b5277eb7cc1458e0797ddc21283e094eb3ad
Athanase Matabaro <athanase678@gmail.com>
2022-05-09 14:04:10 +0300

    class concept init

---
+++ system/main.py
@@ -0,0 +1,10 @@
+item1 = 'phone'
+item1_price = 100
+item1_quantity =5
+item1_price_total = item1_price * item1_quantity
+
+print(type(item1)) #string
+print(type(item1_price)) # int
+print(type(item1_quantity)) # int
+print(type(item1_price_total)) # int 
+
